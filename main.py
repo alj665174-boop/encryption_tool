2d7ccb2cfa54fa062b0e58d36747c6526cb702b7
dell <ezzatabdulhakim32@gmail.com>
2026-02-03 10:53:15 +0300

    edit -by eazzt

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from rsa_tool import generate_rsa_keys, rsa_encrypt, rsa_decrypt
 
 password = "strongpassword123"
 
-encrypted = aes_encrypt(password, "Hello Doctor 👩‍🏫")
-print("Encrypted AES:", encrypted)
+encrypted = aes_encrypt(password, "Hello -team 👩‍🏫")
+print("Encrypted -AES:", encrypted)
 
 decrypted = aes_decrypt(password, encrypted)
-print("Decrypted AES:", decrypted)
+print("Decrypted -AES:", decrypted)
 
 private_key, public_key = generate_rsa_keys()
-cipher = rsa_encrypt(public_key, "RSA Secret")
-print("RSA Decrypted:", rsa_decrypt(private_key, cipher))
+cipher = rsa_encrypt(public_key, "RSA -Secret")
+print("RSA -Decrypted:", rsa_decrypt(private_key, cipher))
